00de72f006666ffbed7ac5ae293b8edef1497158
snowingchair <145107567+snowingchair@users.noreply.github.com>
2023-11-23 14:51:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,19 +3,27 @@ class app
     runApplication()
     {
         this.greeting = "starting up"
+
+        let = boolean = (true);
+        let = String = (50);
+        let = number = (even);
     }
 }
 
 this.app = new app();
 app.runApplication();
 
+
+(this.appNaam = "mijnapp")
+(this.versienummer = "0.1")
+(this.versiedatum  = "21/11/23")
+(this.autheur = "Samuel Enderink")
+(this.copyright = "Samuel Enderink")
+(this.distributeur = "Samuel Enderink")
+(this.darkmode = "true")
+
 console.log(app.greeting);
 console.log("appNaam: " + app.appNaam);
 console.log("versienummer: " + app.versienummer);
-(this,appNaam = "mijnapp")
-(this,versienummer = "0.1")
-(this,versiedatum  = "21/11/23")
-(this,autheur = "Samuel Enderink")
-(this,copyright = "Samuel Enderink")
-(this,distributeur = "Samuel Enderink")
-(this,darkmode = "true")
+console.log("autheur: " + app.autheur);
+console.log("darkmode: " + app.darkmode);
